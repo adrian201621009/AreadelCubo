185ad065f0203d7a99e93e8d15029e67355e94ac
adrian hernandez <fortaleza2012@hotmail.com>
2017-11-07 20:39:28 -0600

    Agregar archivos de proyecto.

---
+++ AreadelCubo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreadelCubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int L;
+            int area, volumen;
+            Console.WriteLine("Ingresa el lado del cubo: ");
+            L = int.Parse(Console.ReadLine());
+            area = 6 * L * L;
+            volumen = L * L * L;
+            Console.WriteLine("El Area es: " + area);
+            Console.WriteLine("El Volumen es: " + volumen);
+            Console.ReadKey();
+        }
+    }
+}
